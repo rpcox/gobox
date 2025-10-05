902bf7667d5345765765487b860b680bea2d1224
Ric Cox <ric.cox@gmail.com>
2025-10-05 08:08:05 -0700

    add cpu elevation

--- systemd/dummy/main.go
+++ systemd/dummy/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
 	"time"
 
@@ -11,21 +12,56 @@ import (
 )
 
 var (
-	tool string = "dummy"
-	version string = "v0.1.0"
+	tool    string = "dummy"
+	version string = "v0.2.0"
 )
 
+func ElevateCPU(done chan bool, id int) {
+
+	fmt.Printf("[%d] enter cpu elevation\n", id)
+	for {
+		select {
+		case <-done:
+			fmt.Printf("[%d] exit cpu elevation\n", id)
+			return
+		default:
+			// Perform a simple, non-blocking calculation
+			_ = 12345 * 67890 / 98765 % 43210
+		}
+	}
+}
 
 func main() {
 
+	fmt.Printf("%s %s pid=%d\n", tool, version, os.Getpid())
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1)
+
 	msgCount := 1
+	cpuElevated := false
+	done := make(chan bool, runtime.NumCPU())
 
 	for {
 		select {
-		case <- sigChan:
-			return
+		case sig := <-sigChan:
+			if sig == syscall.SIGUSR1 {
+				fmt.Println("signal: toggle cpu utilization")
+				if !cpuElevated {
+					for i := 0; i < runtime.NumCPU(); i++ {
+						go ElevateCPU(done, i)
+					}
+					cpuElevated = true
+				} else {
+					for i := 0; i < runtime.NumCPU(); i++ {
+						done <- true
+					}
+				}
+			} else if sig == syscall.SIGTERM || sig == syscall.SIGINT {
+				close(done)
+				close(sigChan)
+				return
+			}
 		default:
 			msg := fmt.Sprintf("dummy message [%d]", msgCount)
 			err := journal.Send(msg, journal.PriInfo, nil)
