1106fbc91eca52be52a1b88ffebb7d3e2beb3bee
Ric Cox <ric.cox@gmail.com>
2025-10-05 15:37:04 -0700

    add flag package

--- systemd/dummy/main.go
+++ systemd/dummy/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"math"
 	"os"
 	"os/signal"
 	"runtime"
@@ -45,9 +47,30 @@ func JournalMessage(exit, mark chan bool) {
 }
 
 func main() {
+	_cpu_check := flag.Bool("cpu-check", false, "Determine the number of system CPUs")
+	_speed := flag.Float64("speed", 1.0, "The fraction of CPUs to use")
+	flag.Parse()
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	if *_cpu_check {
+		fmt.Fprintf(os.Stderr, "cpu count = %d", runtime.NumCPU())
+		os.Exit(0)
+	}
+
+	var max_cores int
+	if *_speed < 0 || *_speed > 1.0 {
+		fmt.Fprintf(os.Stderr, "speed must be > 0 and <= 1\n")
+		os.Exit(1)
+	} else {
+		max_cores = int(math.Ceil(float64(runtime.NumCPU()) / *_speed))
+		if max_cores < 1 {
+			max_cores = 1
+		}
+	}
 
 	fmt.Fprintf(os.Stderr, "%s %s pid=%d\n", tool, version, os.Getpid())
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	fmt.Fprintf(os.Stderr, "cpu_count=%d\n", runtime.NumCPU())
+
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1, syscall.SIGUSR2)
 
@@ -64,12 +87,12 @@ func main() {
 			if sig == syscall.SIGUSR1 {
 				fmt.Fprintf(os.Stderr, "sigusr1: toggle cpu utilization\n")
 				if !cpuElevated {
-					for i := 1; i <= runtime.NumCPU(); i++ {
+					for i := 1; i <= max_cores; i++ {
 						go ElevateCPU(done, i)
 					}
 					cpuElevated = true
 				} else {
-					for i := 1; i <= runtime.NumCPU(); i++ {
+					for i := 1; i <= max_cores; i++ {
 						done <- true
 					}
 					cpuElevated = false
