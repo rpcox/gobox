8c359c08e033b566288f4404955aff6b497b96cf
Ric Cox <ric.cox@gmail.com>
2025-10-05 06:35:33 -0700

    add key usage

--- crypto/cert-info/main.go
+++ crypto/cert-info/main.go
@@ -9,58 +9,93 @@ import (
 )
 
 func main() {
-	_file := flag.String("file", "", "Identify the X509 certificate")
-	_altNames := flag.Bool("alt-names", false, "Show subject alternative names")
-	_rem := flag.Bool("rem", false, "Show any remaining text")
+	_algo := flag.Bool("algo", false, "Show public and signature algorithms")
 	_crl := flag.Bool("crl", false, "Show any CRL distribution points")
+	_rem := flag.Bool("rem", false, "Show any remaining text")
+	_san := flag.Bool("san", false, "Show subject alternative names")
+	_ski := flag.Bool("ski", false, "Show subject key identifer")
+	_ku := flag.Bool("key-usage", false, "Show subject key identifer")
 	flag.Parse()
 
-	if *_file == "" {
-		fmt.Fprintf(os.Stderr, "-file required\n")
-		os.Exit(1)
-	}
+	files := flag.Args()
+	for _, file := range files {
+		data, err := os.ReadFile(file)
+		if err != nil {
+			fmt.Printf("error reading file %s:  %v\n", file, err)
+			continue
+		}
 
-	data, err := os.ReadFile(*_file)
-	if err != nil {
-		fmt.Printf("error reading file %s:  %v\n", *_file, err)
-		os.Exit(1)
-	}
+		block, rest := pem.Decode(data)
+		if block == nil || block.Type != "CERTIFICATE" {
+			fmt.Println("Failed to decode PEM block containing certificate")
+			os.Exit(1)
+		}
 
-	block, rest := pem.Decode(data)
-	if block == nil || block.Type != "CERTIFICATE" {
-		fmt.Println("Failed to decode PEM block containing certificate")
-		os.Exit(1)
-	}
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			fmt.Printf("Failed to parse certificate: %v\n", err)
+			return
+		}
 
-	cert, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		fmt.Printf("Failed to parse certificate: %v\n", err)
-		return
-	}
+		fmt.Printf("      File: %s\n", file)
+		fmt.Printf("   Subject: %s\n", cert.Subject.String())
+		fmt.Printf("   Version: %d\n", cert.Version)
+		fmt.Printf("    Serial: %s\n", cert.SerialNumber.String())
+		fmt.Printf("    Issuer: %s\n", cert.Issuer.String())
+		fmt.Printf("     Start: %s\n", cert.NotBefore.String())
+		fmt.Printf("   Expires: %s\n", cert.NotAfter.String())
 
-	fmt.Println("Certificate Details:")
-	fmt.Printf("   Subject: %s\n", cert.Subject.String())
-	fmt.Printf("   Version: %d\n", cert.Version)
-	fmt.Printf("    Serial: %s\n", cert.SerialNumber.String())
-	fmt.Printf("    Issuer: %s\n", cert.Issuer.String())
-	fmt.Printf("     Start: %s\n", cert.NotBefore.String())
-	fmt.Printf("   Expires: %s\n", cert.NotAfter.String())
-	fmt.Printf("   SigAlgo: %s\n", cert.SignatureAlgorithm.String())
-	fmt.Printf("PubKeyAlgo: %s\n", cert.PublicKeyAlgorithm.String())
-	fmt.Printf(" Key Usage: %v\n", cert.KeyUsage)
-	if *_altNames {
-		fmt.Printf("      SAN:\n%v\n", cert.DNSNames)
-	}
+		if *_algo {
+			fmt.Printf("PubKeyAlgo: %s\n", cert.PublicKeyAlgorithm.String())
+			fmt.Printf("   SigAlgo: %s\n", cert.SignatureAlgorithm.String())
+		}
 
-	if *_rem {
-		fmt.Printf("  Remnant: %q\n", rest)
-	}
+		if *_crl {
+			fmt.Printf("       CRL:\n")
+			for _, v := range cert.CRLDistributionPoints {
+				fmt.Printf("          %v\n", v)
+			}
+		}
 
-	if *_crl {
-	        fmt.Printf("       CRL:\n")
-		for _, v := range cert.CRLDistributionPoints {
-	        	fmt.Printf("          %v\n", v)
-	}
-		fmt.Printf("  Remnant: %q\n", rest)
+		if *_ku {
+			ku := make(map[x509.KeyUsage]string)
+			ku[x509.KeyUsageDigitalSignature] = "Digital Signature"
+			ku[x509.KeyUsageContentCommitment] = "Content Commitment"
+			/*ku[3] = "Key Encipherment"
+			ku[4] = "Data Encipherment"
+			ku[5] = "Key Agreement"
+			ku[6] = "Cert Sign"
+			ku[7] = "CRL Sign"
+			ku[8] = "Encipher Only"
+			ku[9] = "Decipher Only"*/
+
+			fmt.Print(" Key Usage: ")
+			var i x509.KeyUsage
+			first := true
+			for i = x509.KeyUsageDigitalSignature; i <= x509.KeyUsageDecipherOnly; i++ {
+				if or := i | cert.KeyUsage; or == 1 {
+					if first {
+						first = false
+						fmt.Printf("%s\n", ku[i])
+					} else {
+						fmt.Printf("           %s\n", ku[i])
+					}
+				}
+			}
+		}
+
+		if *_rem {
+			fmt.Printf("  Remnant: %q\n", rest)
+		}
+
+		if *_san {
+			fmt.Printf("       SAN:\n%v\n", cert.DNSNames)
+		}
+
+		if *_ski {
+			fmt.Printf(" Subject Key ID: %v\n", cert.SubjectKeyId)
+		}
+
+		fmt.Println()
 	}
 }
